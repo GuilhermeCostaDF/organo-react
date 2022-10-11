1e413042655765502bf23a4d0930595ba1664a64
GuidoCostaDF <guidocostadf@gmail.com>
2022-10-11 12:30:43 -0300

    Componente Rodape criado

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,21 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <footer className="rodape">
+      <div className="redes">
+        <img src="/imagens/fb.png" alt="logo" />
+        <img src="/imagens/tw.png" alt="logo" />
+        <img src="/imagens/ig.png" alt="logo" />
+      </div>
+      <div className="logo">
+        <img src="/imagens/logo.png" alt="logo" />
+      </div>
+      <div className="direitos">
+        <p>&#169; Desenvolvido por Alura</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Rodape;
